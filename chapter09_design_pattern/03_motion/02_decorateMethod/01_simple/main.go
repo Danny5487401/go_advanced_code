4e6809c09d2a63c435c35198be1ddba3e059f07d
xiaxin <xiaxin5487401@gmail.com>
2021-06-02 14:53:15 +0800

    decoration

--- chapter09_design_pattern/03_motion/02_decorateMethod/01_simple/main.go
+++ chapter09_design_pattern/03_motion/02_decorateMethod/01_simple/main.go
@@ -1,30 +1,31 @@
 package main
 
+// 装饰模式
 import "fmt"
 
-type Decorator func(i int,s string) bool
+type Decorator func(i int, s string) bool
 
 // 被装饰的函数
-func foo(i int,s string) bool {
+func foo(i int, s string) bool {
 	fmt.Println("___foo___")
 	return true
 }
 
 // 增加的功能，装饰器
-func withDeco(fn Decorator)Decorator  {
+func withDeco(fn Decorator) Decorator {
 	return func(i int, s string) bool {
 		fmt.Println("开始装饰")
-		result := fn(i,s)
+		result := fn(i, s)
 		fmt.Println("结束装饰")
 		return result
 	}
 }
 
 // 开始使用
-func main()  {
+func main() {
 	// 开始用装饰器修饰自己
 	foo := withDeco(foo)
 	// 调用自己
-	foo(1,"hello danny")
-	
+	foo(1, "hello danny")
+
 }
