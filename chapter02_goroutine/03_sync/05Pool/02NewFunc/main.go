8ede89ed99d34d21125e888af0636f9556360d4e
danny <540021730@qq.com>
2021-06-22 11:02:32 +0800

    sync_pool process of getting and putting

--- chapter02_goroutine/03_sync/05Pool/02NewFunc/main.go
+++ chapter02_goroutine/03_sync/05Pool/02NewFunc/main.go
@@ -7,9 +7,9 @@ import (
 	"sync"
 )
 
-func main()  {
+func main() {
 	// 建立对象
-	var pipe = &sync.Pool{New:func()interface{}{return "Hello, danny"}}
+	var pipe = &sync.Pool{New: func() interface{} { return "Hello, danny" }}
 
 	// 准备放入的字符串
 	val := "Hello,World!"
@@ -19,10 +19,16 @@ func main()  {
 
 	// 取出
 	first := pipe.Get().(string)
-	fmt.Println(first)
+	fmt.Println(first) // Hello,World!
 
 	// 再取就没有了,会自动调用NEW
 	second := pipe.Get().(string)
-	fmt.Println(second)
+	fmt.Println(second) // Hello, danny
 }
 
+/*
+
+
+
+
+ */
--- chapter02_goroutine/03_sync/05Pool/introduce.go
+++ chapter02_goroutine/03_sync/05Pool/introduce.go
@@ -23,7 +23,7 @@ func main() {
 	a := p.Get().(int)
 	p.Put(1)
 
-	//runtime.GC()   //注意注释前后的结果
+	//runtime.GC() //注意注释前后的结果
 
 	b := p.Get().(int)
 	fmt.Println(a, b)
@@ -31,14 +31,23 @@ func main() {
 }
 
 /*
-上面我们可以看到pool创建的时候是不能指定大小的，所有sync.Pool的缓存对象数量是没有限制的（只受限于内存），因此使用sync.pool是没办法做到控制缓存对象数量的个数的。
-	另外sync.pool缓存对象的期限是很诡异的，这是很多人错误理解的地方，
+上面我们可以看到pool创建的时候是不能指定大小的，所有sync.Pool的缓存对象数量是没有限制的（只受限于内存），
+	因此使用sync.pool是没办法做到控制缓存对象数量的个数的。另外sync.pool缓存对象的期限是很诡异的，这是很多人错误理解的地方，
 
-func init() {
-    runtime_registerPoolCleanup(poolCleanup)
-}
-可以看到pool包在init的时候注册了一个poolCleanup函数，它会清除所有的pool里面的所有缓存的对象，该函数注册进去之后会在每次gc之前都会调用，因此sync.Pool缓存的期限只是两次gc之间这段时间
+源码分析
+	sync.Pool 的 init 函数
+		func init() {
+			runtime_registerPoolCleanup(poolCleanup)
+		}
+		func runtime_registerPoolCleanup(cleanup func())
+	可以看到pool包在init的时候注册了一个poolCleanup函数，它会清除所有的pool里面的所有缓存的对象，该函数注册进去之后会在每次gc之前都会调用，
+		因此sync.Pool缓存的期限只是两次gc之间这段时间
 
-正因为这样，我们是不可以使用sync.Pool去实现一个socket连接池的。
+	正因为这样，我们是不可以使用sync.Pool去实现一个socket连接池的。
+
+看sync_pool_structure.png流程
+	一个goroutine固定在一个局部调度器P上，从当前 P 对应的 poolLocal 取值， 若取不到，则从对应的 shared 数组上取，若还是取不到；
+	则尝试从其他 P 的 shared 中偷。 若偷不到，则调用 New 创建一个新的对象。池中所有临时对象在一次 GC 后会被全部清空。
+
 
 */
