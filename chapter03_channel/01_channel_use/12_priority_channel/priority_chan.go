60307200fd0f6698854e72b8a0f2a3cfc39ca40d
danny <540021730@qq.com>
2022-01-06 21:46:20 +0800

    fix(展示): markdown优化

--- chapter03_channel/01_channel_use/12_priority_channel/priority_chan.go
+++ chapter03_channel/01_channel_use/12_priority_channel/priority_chan.go
@@ -2,7 +2,7 @@ package _2_priority_channel
 
 import "fmt"
 
-func worker(ch1, ch2 <-chan int, stopCh chan struct{}) {
+func worker1(ch1, ch2 <-chan int, stopCh chan struct{}) {
 
 	for {
 		select {
@@ -16,6 +16,30 @@ func worker(ch1, ch2 <-chan int, stopCh chan struct{}) {
 				fmt.Println(job2)
 			default:
 			}
+		}
+	}
+}
+
+// Note:worker1,如果ch1和ch2都没有达到就绪状态的话，整个程序不会阻塞而是进入了死循环
+
+func worker2(ch1, ch2 <-chan int, stopCh chan struct{}) {
+	for {
+		select {
+		case <-stopCh:
+			return
+		case job1 := <-ch1:
+			fmt.Println(job1)
+		case job2 := <-ch2:
+		priority:
+			for {
+				select {
+				case job1 := <-ch1:
+					fmt.Println(job1)
+				default:
+					break priority
+				}
+			}
+			fmt.Println(job2)
 		}
 	}
 }
