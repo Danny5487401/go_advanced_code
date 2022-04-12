641f17f3c6e2691fd5d5d4fd0754dc7f12445806
danny <540021730@qq.com>
2022-04-12 16:53:18 +0800

    feat(c): 动态连接库后缀名介绍

--- chapter13_Go_call_C_or_C++/10_dynamic_c_lib/main.go
+++ chapter13_Go_call_C_or_C++/10_dynamic_c_lib/main.go
@@ -1,7 +1,7 @@
 package main
 
 //#cgo CFLAGS: -I./number
-//#cgo LDFLAGS: -L./number -lnumber
+//#cgo LDFLAGS: -L"./number" -lnumber
 //#include "number.h"
 import "C"
 import "fmt"
@@ -9,3 +9,8 @@ import "fmt"
 func main() {
 	fmt.Println(C.number_add_mod(10, 6, 12))
 }
+
+// 需要注意的是，在运行时需要将动态库放到系统能够找到的位置。
+//1. 对于windows来说，可以将动态库和可执行程序放到同一个目录，或者将动态库所在的目录绝对路径添加到PATH环境变量中，动态链接库后缀为 .dll
+//2. 对于macOS来说，需要设置DYLD_LIBRARY_PATH环境变量，动态链接库.dylib
+//3. 而对于Linux系统来说，需要设置LD_LIBRARY_PATH环境变量，动态链接库后缀.so
