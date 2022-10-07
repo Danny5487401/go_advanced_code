29402546e25960ea891b9d49f2ea0ed71848e0df
danny <540021730@qq.com>
2022-10-07 20:46:59 +0800

    feat(https及tls): 证书生成及使用

--- chapter17_dataStructure_n_algorithm/06_certificate/02_x509/main.go
+++ chapter17_dataStructure_n_algorithm/06_certificate/02_x509/main.go
@@ -1,86 +1,152 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Generate a self-signed X.509 certificate for a TLS server. Outputs to
+// 'cert.pem' and 'key.pem' and will overwrite existing files.
+
 package main
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
-	"io/ioutil"
+	"encoding/pem"
+	"fmt"
+	flag "github.com/spf13/pflag"
 	"log"
 	"math/big"
+	"os"
 	"time"
 )
 
+var (
+	emailAddress = flag.StringP("email-address", "e", "", "The email address of the user you wish to create the certificate for")
+	validFrom    = flag.String("start-date", "", "Creation date formatted as Jan 1 15:04:05 2011")
+	validFor     = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
+	isCA         = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
+	rsaBits      = flag.Int("rsa-bits", 2048, "Size of RSA key to generate. Ignored if --ecdsa-curve is set")
+	ecdsaCurve   = flag.String("ecdsa-curve", "", "ECDSA curve to use to generate a key. Valid values are P224, P256, P384, P521")
+)
+
+func publicKey(priv interface{}) interface{} {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &k.PublicKey
+	case *ecdsa.PrivateKey:
+		return &k.PublicKey
+	default:
+		return nil
+	}
+}
+
+func pemBlockForKey(priv interface{}) *pem.Block {
+	switch k := priv.(type) {
+	case *rsa.PrivateKey:
+		return &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(k)}
+	case *ecdsa.PrivateKey:
+		b, err := x509.MarshalECPrivateKey(k)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to marshal ECDSA private key: %v", err)
+			os.Exit(2)
+		}
+		return &pem.Block{Type: "EC PRIVATE KEY", Bytes: b}
+	default:
+		return nil
+	}
+}
+
 func main() {
-	// ca 证书
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(1653),
+	flag.Parse()
+
+	if len(*emailAddress) == 0 {
+		log.Fatalf("Missing required --email-address parameter")
+	}
+
+	var priv interface{}
+	var err error
+	switch *ecdsaCurve {
+	case "":
+		priv, err = rsa.GenerateKey(rand.Reader, *rsaBits)
+	case "P224":
+		priv, err = ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
+	case "P256":
+		priv, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	case "P384":
+		priv, err = ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	case "P521":
+		priv, err = ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	default:
+		fmt.Fprintf(os.Stderr, "Unrecognized elliptic curve: %q", *ecdsaCurve)
+		os.Exit(1)
+	}
+	if err != nil {
+		log.Fatalf("failed to generate private key: %s", err)
+	}
+
+	var notBefore time.Time
+	if len(*validFrom) == 0 {
+		notBefore = time.Now()
+	} else {
+		notBefore, err = time.Parse("Jan 2 15:04:05 2006", *validFrom)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse creation date: %s\n", err)
+			os.Exit(1)
+		}
+	}
+
+	notAfter := notBefore.Add(*validFor)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		log.Fatalf("failed to generate serial number: %s", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			Country:            []string{"China"},
-			Organization:       []string{"Devops"},
-			OrganizationalUnit: []string{"unit1"},
+			Country:      []string{"CN"},
+			Organization: []string{"Acme Company"},
 		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(10, 0, 0),
-		SubjectKeyId:          []byte{1, 2, 3, 4, 5},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage: x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth,
+			x509.ExtKeyUsageClientAuth},
 		BasicConstraintsValid: true,
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 	}
-	// 私钥及公钥 rsa 格式
-	caSelfSignedPrivateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
-	caSelfSignedPublicKey := &caSelfSignedPrivateKey.PublicKey
-	// 自签证书 []byte
-	caSelfSigned, err := x509.CreateCertificate(rand.Reader, ca, ca, caSelfSignedPublicKey, caSelfSignedPrivateKey)
+	template.DNSNames = append(template.DNSNames, "tencent.danny.games", "localhost")
+	template.EmailAddresses = append(template.EmailAddresses, *emailAddress)
+
+	if *isCA {
+		template.IsCA = true
+		template.KeyUsage |= x509.KeyUsageCertSign
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, publicKey(priv), priv)
 	if err != nil {
-		log.Println("create ca failed", err)
+		log.Fatalf("Failed to create certificate: %s", err)
+	}
+
+	certOut, err := os.Create("chapter17_dataStructure_n_algorithm/06_certificate/02_x509/cert.pem")
+	if err != nil {
+		log.Fatalf("failed to open cert.pem for writing: %s", err)
+	}
+	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	certOut.Close()
+	log.Print("written cert.pem\n")
+
+	keyOut, err := os.OpenFile("chapter17_dataStructure_n_algorithm/06_certificate/02_x509/key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Print("failed to open key.pem for writing:", err)
 		return
 	}
-	caSelfSignedFile := "ca.pem"
-	log.Println("write to", caSelfSignedFile)
-	ioutil.WriteFile(caSelfSignedFile, caSelfSigned, 0777) // 将自签证书写入文件
-
-	caSelfSignedPrivateKeyFile := "ca.key"
-	caSelfSignedPrivateKeyDER := x509.MarshalPKCS1PrivateKey(caSelfSignedPrivateKey) // 将私钥转换为 DER 格式
-	log.Println("write to", caSelfSignedPrivateKeyFile)
-	ioutil.WriteFile(caSelfSignedPrivateKeyFile, caSelfSignedPrivateKeyDER, 0777) // 将 DER 编码私钥写入文件
-
-	// 待签署证书及其私钥公钥
-	cert := &x509.Certificate{
-		SerialNumber: big.NewInt(1658),
-		Subject: pkix.Name{
-			Country:            []string{"China"},
-			Organization:       []string{""},
-			OrganizationalUnit: []string{""},
-		},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-	}
-	certPrivateKey, _ := rsa.GenerateKey(rand.Reader, 1024)
-	certPublicKey := &certPrivateKey.PublicKey
-
-	// 使用自签CA 对 证书签署
-	certSigned, err2 := x509.CreateCertificate(rand.Reader, cert, ca, certPublicKey, caSelfSignedPrivateKey)
-	if err != nil {
-		log.Println("create cert2 failed", err2)
-		return
-	}
-
-	certFile := "cert.pem"
-	log.Println("write to", certFile)
-	ioutil.WriteFile(certFile, certSigned, 0777) // cert 写入文件
-
-	certPrivateKeyFile := "cert.key"
-	certPrivateKeyDER := x509.MarshalPKCS1PrivateKey(certPrivateKey) // 将私钥转换为 DER 编码格式
-	log.Println("write to", certPrivateKeyFile)
-	ioutil.WriteFile(certPrivateKeyFile, certPrivateKeyDER, 0777) // 私钥写入文件
-
-	caTr, _ := x509.ParseCertificate(caSelfSigned)
-	certTr, _ := x509.ParseCertificate(certSigned)
-	err = certTr.CheckSignatureFrom(caTr) // 验证 c 上的签名是否是来自 parent 的有效签名
-	log.Println("check signature", err)
+	pem.Encode(keyOut, pemBlockForKey(priv))
+	keyOut.Close()
+	log.Print("written key.pem\n")
 }
